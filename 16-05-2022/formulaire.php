617f718e5a0f230dd0df86912ead0766c82e79e3
jonathan YHWH <alphajorider@gmail.com>
2022-05-17 11:02:03 +0200

    réussi
    résultat final

--- 16-05-2022/formulaire.php
+++ 16-05-2022/formulaire.php
@@ -8,13 +8,17 @@
     <title>formulaire</title>
 </head>
 <body>
-    <form action="formulaire.php" method="POST">
+    <section class="main">
+        <h1>Livre d'or</h1>
+        <form action="formulaire.php" method="POST">
         <input type="text" name="nom" placeholder="nom" required maxlength="25" class="nom">
         <input type="text" name="objet" placeholder="objet" required maxlength="25" class="objet">
-        <textarea name="message" id="message" cols="30" rows="10" maxlength="250" class="message"></textarea>
-        <input type="submit" value="valider" name="valider" class="valider">
-        <input type="reset" value="annuler" class="annuler">
+        <textarea name="message" id="message" cols="40" rows="10" maxlength="250" class="message" placeholder="écrire un message"></textarea>
+        <input type="submit" value="valider" name="valider" class="valider bas">
+        <input type="reset" value="annuler" class="annuler bas">
     </form>
+    </section>
+    
 
     <?php
         if (isset($_POST['message'])&isset($_POST['nom'])&isset($_POST['objet'])){
@@ -27,23 +31,26 @@
             $fileopen=(fopen("$fichier",'a'));
             // fputs($fileopen, "\n");
             # Ecriture de "Début du fichier" dansle fichier texte
-            fwrite($fileopen,('Nom : '.$nom. ' <br/> ' .'Objet : '.$objet. ' <br/> ' .'Message :'.$message. ' <br/> ' .'Date :'.date("j,F, Y, g:i a")));
+            fwrite($fileopen,('Nom : '.$nom.'<br/>'.'Objet : '.$objet.'<br/>'.'Message :'.$message.'<br/>'.'Date :'.date("j,F, Y, g:i a")));
             # On ferme le fichier proprement
             fputs($fileopen, "\n");
             fclose($fileopen);
         } 
     ?>
-    <table>
-        <?php
-            $contents=file_get_contents("./txt/formulaire.txt");
-            $lignes=explode("\n",$contents);
+    <!-- <section class="affichage"> -->
+        <table>
+            <?php
+                $contents=file_get_contents("./txt/formulaire.txt");
+                $lignes=explode("\n",$contents);
 
-            foreach($lignes as $ligne){
-            echo '<tr>'.'<td>';
-            echo $ligne;
-            echo '</td>'.'</tr>';
-            }
-        ?>
-    </table>
+                foreach($lignes as $ligne){
+                echo '<tr>'.'<td>';
+                echo $ligne;
+                echo '</td>'.'</tr>';
+                }
+            ?>
+        </table> 
+    <!-- </section> -->
+
 </body>
 </html>
