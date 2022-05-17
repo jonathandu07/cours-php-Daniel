90481e4df6bc905b2878f3030eab41c82fb96a33
jonathan YHWH <alphajorider@gmail.com>
2022-05-17 09:48:42 +0200

    fini

--- 16-05-2022/formulaire.php
+++ 16-05-2022/formulaire.php
@@ -9,11 +9,11 @@
 </head>
 <body>
     <form action="formulaire.php" method="POST">
-        <input type="text" name="nom" placeholder="nom" required maxlength="25">
-        <input type="text" name="objet" placeholder="objet" required maxlength="25">
-        <textarea name="message" id="message" cols="30" rows="10" maxlength="250"></textarea>
-        <input type="submit" value="valider" name="valider">
-        <input type="reset" value="annuler">
+        <input type="text" name="nom" placeholder="nom" required maxlength="25" class="nom">
+        <input type="text" name="objet" placeholder="objet" required maxlength="25" class="objet">
+        <textarea name="message" id="message" cols="30" rows="10" maxlength="250" class="message"></textarea>
+        <input type="submit" value="valider" name="valider" class="valider">
+        <input type="reset" value="annuler" class="annuler">
     </form>
 
     <?php
@@ -27,22 +27,22 @@
             $fileopen=(fopen("$fichier",'a'));
             // fputs($fileopen, "\n");
             # Ecriture de "Début du fichier" dansle fichier texte
-            fwrite($fileopen,('Nom : '.$nom.' '.'Objet : '.$objet.$message.date("j,F, Y, g:i a")));
+            fwrite($fileopen,('Nom : '.$nom. ' <br/> ' .'Objet : '.$objet. ' <br/> ' .'Message :'.$message. ' <br/> ' .'Date :'.date("j,F, Y, g:i a")));
             # On ferme le fichier proprement
             fputs($fileopen, "\n");
             fclose($fileopen);
         } 
-
     ?>
-    <!-- <a href="formulaire.php?nom=<?= rawurlencode($nom) ?>">Publier message</a> -->
     <table>
         <?php
-        $contents=file_get_contents("./txt/formulaire.txt");
-        $lines=explode("\n",$contents);
-        foreach($lines as $line){
-        echo $line.'<br>';
+            $contents=file_get_contents("./txt/formulaire.txt");
+            $lignes=explode("\n",$contents);
 
-        }
+            foreach($lignes as $ligne){
+            echo '<tr>'.'<td>';
+            echo $ligne;
+            echo '</td>'.'</tr>';
+            }
         ?>
     </table>
 </body>
